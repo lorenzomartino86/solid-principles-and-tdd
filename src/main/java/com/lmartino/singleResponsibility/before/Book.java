e6e3c36e6349bf8dccbc7cede06fb19af5d8b77e
lore <lorenzomartino86@gmail.com>
2018-01-13 00:12:50 +0100

    initial commit

---
+++ src/main/java/com/lmartino/singleResponsibility/before/Book.java
@@ -0,0 +1,47 @@
+package main.java.com.lmartino.singleResponsibility.before;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by lore on 13/01/18.
+ */
+public class Book {
+    private String title;
+    private String author;
+    private List<String> pages;
+    private int currentPageIndex = 0;
+
+    public Book(String title, String author, ArrayList<String> pages) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void turnPage() {
+        if (currentPageIndex < pages.size() - 1) {
+            currentPageIndex++;
+        }
+    }
+
+    public String printCurrentPage(String displayType) {
+        if (displayType.equals("plainText")) {
+            return pages.get(currentPageIndex);
+        } else if (displayType.equals("html")) {
+            return "<div class='page'>" + pages.get(currentPageIndex) + "</div>";
+        }
+        return "Unknown type";
+    }
+
+    public int getCurrentPage() {
+        return currentPageIndex + 1;
+    }
+}
---
+++ src/test/java/com/lmartino/singleResponsibility/after/BookTest.java
@@ -0,0 +1,10 @@
+package com.lmartino.singleResponsibility.after;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by lore on 13/01/18.
+ */
+public class BookTest {
+
+}
