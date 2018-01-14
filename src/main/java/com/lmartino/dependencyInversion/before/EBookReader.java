4cfdfaaafd9a4bcf4ea795c723b3f08864fad0ff
lore <lorenzomartino86@gmail.com>
2018-01-14 17:49:48 +0100

    preparing dependency inversion

---
+++ src/main/java/com/lmartino/dependencyInversion/before/EBookReader.java
@@ -0,0 +1,15 @@
+package com.lmartino.dependencyInversion.before;
+
+public class EBookReader {
+
+	private PDFBook book;
+	
+	public EBookReader(PDFBook pdfBook) {
+		this.book = pdfBook;
+	}
+
+	public String read() {
+		return book.read();
+	}
+	
+}
---
+++ src/main/java/com/lmartino/dependencyInversion/before/PDFBook.java
@@ -0,0 +1,9 @@
+package com.lmartino.dependencyInversion.before;
+
+public class PDFBook {
+
+	public String read() {
+		return "Reading a PDF Book...";
+	}
+
+}
