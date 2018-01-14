baa4f262917341dc5225ec2d485470618c5568cf
lore <lorenzomartino86@gmail.com>
2018-01-14 16:20:09 +0100

    cleaned up unused method

--- src/main/java/com/lmartino/liskovSubstitution/after/RubberDucky.java
+++ src/main/java/com/lmartino/liskovSubstitution/after/RubberDucky.java
@@ -2,10 +2,6 @@ package com.lmartino.liskovSubstitution.after;
 
 public class RubberDucky implements Duck {
 
-	public void fly() {
-		throw new UnsupportedOperationException();
-	}
-
 	public void quack() {
 		System.out.println("Quack! Quack! Quack!");
 	}	
